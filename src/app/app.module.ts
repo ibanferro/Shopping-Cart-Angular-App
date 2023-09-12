f04eafcdcb2810a5077e2e46a888394d8f63491c
ibanferro <iban.ferro@gmail.com>
2023-09-12 23:00:09 +0200

    Main components initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { SearchboxComponent } from './components/searchbox/searchbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    SearchboxComponent
   ],
   imports: [
     BrowserModule,
